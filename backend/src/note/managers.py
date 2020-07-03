fe4f62b38720cc6b5fab8d2b5952776b4f46fa77
Estela Romero Muñoz <estelaromer@gmail.com>
2020-07-03 11:36:52 +0200

    Manager method to hard delete all items

--- backend/src/note/managers.py
+++ backend/src/note/managers.py
@@ -4,3 +4,7 @@ from django.db import models
 class RealManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(deleted='False')
+
+    def hard_delete(self):
+        for item in self.all():
+            item.hard_delete()
