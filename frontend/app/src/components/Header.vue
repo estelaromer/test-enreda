78c22ccb05be1d0241bb8f31fe1372c4118adf9f
Estela Romero Muñoz <estelaromer@gmail.com>
2020-07-05 13:24:24 +0200

    Initial components to define the skeleton of the vue app

---
+++ frontend/app/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      title: 'Note App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  margin: auto;
+}
+
+header h1 {
+  font-size: 2.5em;
+}
+</style>
