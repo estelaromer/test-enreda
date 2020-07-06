f02b1d94fb75c6fd92ceb9eb22e344f2174a009f
Estela Romero Muñoz <estelaromer@gmail.com>
2020-07-06 18:26:30 +0200

    django-core-headers and implementation to enable cors-headers adding localhost:8081 to allowed urls

--- backend/src/enreda/settings.py
+++ backend/src/enreda/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'note',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
 
 ROOT_URLCONF = 'enreda.urls'
 
